73bf93209710dc20d9151683e3f3216d4d261577
eslams3dawi <eslamseadawy717@gmail.com>
2025-03-09 18:03:34 +0200

    initial

---
+++ DEPI TASKS DAY 2/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DEPI_TASKS_DAY_2
+{
+    internal class Program
+    {
+        static void Menu()
+        {
+            Console.WriteLine("[1] Add");
+            Console.WriteLine("[2] Substract");
+            Console.WriteLine("[3] Multiple");
+            Console.WriteLine("[4] Divide");
+            Console.WriteLine("[5] Modules\n");
+            Console.WriteLine("[6] Exit");
+        }
+
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write($"Enter Number One : ");
+                int NumberOne = int.Parse(Console.ReadLine());
+
+                Console.Write($"Enter Number Two : ");
+                int NumberTwo = int.Parse(Console.ReadLine());
+                int Choice = 0;
+                Menu();
+                Console.Write("Choose an operation : ");
+                var Input = Console.ReadLine();
+                if (int.TryParse(Input, out Choice))
+                    Choice = int.Parse(Input);
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+
+                switch (Choice)
+                {
+                    case 1:
+                        Console.WriteLine($"{NumberOne} + {NumberTwo} = {NumberOne + NumberTwo}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{NumberOne} - {NumberTwo} = {NumberOne - NumberTwo}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{NumberOne} * {NumberTwo} = {NumberOne * NumberTwo}");
+                        break;
+                    case 4:
+                        Console.WriteLine($"{NumberOne} / {NumberTwo} = {NumberOne / NumberTwo}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"{NumberOne} % {NumberTwo} = {NumberOne % NumberTwo}");
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+#region Switch_TaskDay2
+//enum DaysOfWeek
+//{
+//    None = 0,
+//    Sunday = 1,
+//    Monday = 2,
+//    Tuesday = 3,
+//    Wednesday = 4,
+//    Thursday = 5,
+//    Friday = 6,
+//    Saturday = 7,
+//}
+
+//static void Main(string[] args)
+//{
+//    Console.Write("Enter the number of the day : ");
+//    var Input = Console.ReadLine();
+//    int DayOfTheWeek;
+//    if (int.TryParse(Input, out DayOfTheWeek))
+//        DayOfTheWeek = int.Parse(Input);
+//    else
+//        Console.WriteLine("Invalid Input");
+
+//    switch (DayOfTheWeek)
+//    {
+//        case 1:
+//            Console.WriteLine(DaysOfWeek.Sunday);
+//            break;
+//        case 2:
+//            Console.WriteLine(DaysOfWeek.Monday);
+//            break;
+//        case 3:
+//            Console.WriteLine(DaysOfWeek.Tuesday);
+//            break;
+//        case 4:
+//            Console.WriteLine(DaysOfWeek.Wednesday);
+//            break;
+//        case 5:
+//            Console.WriteLine(DaysOfWeek.Thursday);
+//            break;
+//        case 6:
+//            Console.WriteLine(DaysOfWeek.Friday);
+//            break;
+//        case 7:
+//            Console.WriteLine(DaysOfWeek.Saturday);
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid option");
+//            break;
+//    }
+
+#endregion
+
+
+
+
+
+
+
+#region Mul-table_TaksDay2
+//for(int OperandOne = 1; OperandOne <= 12; OperandOne++)
+//{
+//    for(int OperandTwo = 1; OperandTwo <= 12; OperandTwo++)
+//    {
+//        Console.WriteLine($"{OperandOne} * {OperandTwo} = {OperandOne * OperandTwo}");
+//    }
+//} 
+#endregion
+
+
+
+
+
+
+
+#region DoWhile_TaskDay2
+//    int Number;
+//do
+//{
+//    Console.Write("Enter number greater than 10 : ");
+//    var Input = Console.ReadLine();
+//    if (int.TryParse(Input, out Number))
+//    {
+//        Number = int.Parse(Input);
+//    }
+//    else
+//    {
+//        Console.WriteLine("Invalid input");
+//        return;
+//    }
+//}
+//while (Number <= 10); 
+#endregion
+
+
+
+
+
+
+
+
+#region CountEvenTaskDay2
+//static void Main()
+//{
+//    Console.Write("Enter the size of your array : ");
+//    var Input = Console.ReadLine();
+//    int Size, Summation = 0;
+//    if (int.TryParse(Input, out Size))
+//        Size = int.Parse(Input);
+//    else
+//        Console.WriteLine("Invalid input");
+//    int[] Numbers = new int[Size];
+//    for(int Index = 0; Index < Size; Index++)
+//    {
+//        Console.WriteLine($"Index {Index}");
+//        Numbers[Index] = int.Parse(Console.ReadLine());
+//    }
+//    foreach(var Number in Numbers)
+//    {
+//        if (Number % 2 == 0)
+//            Summation += Number;
+//    }
+//    Console.Write($"The summation of even numbers in your array : {Summation}");
+//} 
+#endregion
+
+
+
+
+
+
+
+
+
+
+#region ReverseArrayTaskDay2
+//Console.Write("Enter the elements : ");
+//var Elements = Console.ReadLine();
+//var ReversedElements = "";
+//for(int Index = Elements.Length-1; Index >= 0; Index--)
+//{
+//    ReversedElements += Elements[Index];
+//}
+//Console.Write($"The array in reversed way : {ReversedElements}"); 
+#endregion
---
+++ DEPI TASKS DAY 3/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace DEPI_TASKS_DAY_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ReverseMethod
+            //    Console.Write("Enter the text or number to reverse it : ");
+            //    var BeforeReversed = Console.ReadLine();
+            //    Console.WriteLine(Reverse(BeforeReversed));
+            //}
+
+            //static string Reverse(string BeforeReversed)
+            //{
+            //    string AfterReversed = "";
+            //    for(int Index = BeforeReversed.Length - 1; Index >=0; Index--)
+            //    {
+            //        AfterReversed += BeforeReversed[Index];
+            //    }
+            //    return AfterReversed; 
+            #endregion
+
+
+
+            #region CountVowelsMethod
+            //    Console.Write("I'm here to count all vowels in your text, just provide me the text here : ");
+            //    var TextInput = Console.ReadLine();
+            //    Console.WriteLine(CountVowels(TextInput));
+            //}
+            //static int CountVowels(string TextInput)
+            //{
+            //    char[] Vowels = { 'a', 'i', 'o', 'u', 'e' };
+            //    int CountOfVowels = 0;
+            //    for(int I = 0; I < TextInput.Length; I++)
+            //    {
+            //        for(int J = 0; J < Vowels.Length; J++)
+            //        {
+            //            if (TextInput[I] == Vowels[J])
+            //            {
+            //                CountOfVowels++;
+            //            }
+            //        }
+            //    }
+            //    return CountOfVowels; 
+            #endregion
+
+
+
+            #region Palindrome
+            //    Console.Write("Enter the text/number : ");
+            //    var Input = Console.ReadLine();
+            //    Console.WriteLine(IsPalindrome(Input));
+            //}
+
+            //static bool IsPalindrome(string input)
+            //{
+            //    bool check = true;
+            //    for(int Index = 0; Index < input.Length/2; Index++)
+            //    {
+            //        if (input[Index] != input[input.Length - 1 - Index])
+            //        {
+            //            check = false;
+            //            break;
+            //        }
+            //    }
+            //    return check;
+            //} 
+            #endregion
+
+
+
+            #region MinMethod
+            //    Console.Write("How many numbers in your array ? : ");
+            //    var Input = Console.ReadLine();
+            //    int SizeArray;
+            //    if (int.TryParse(Input, out SizeArray))
+            //        SizeArray = int.Parse(Input);
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid input");
+            //        return;
+            //    }
+            //    int[] Numbers = new int[SizeArray];
+            //    string input;
+            //    Console.Write("Enter the numbers : ");
+            //    for (int Index = 0; Index < SizeArray; Index++)
+            //    {
+            //        input = Console.ReadLine();
+            //        Numbers[Index] = int.Parse(input);
+            //    }
+
+            //    Console.WriteLine($"The smallest number in your array is [{FindMinimum(Numbers)}]");
+            //}
+
+            //static int FindMinimum(int[] numbers)
+            //{
+            //    int MinValue = numbers[0];
+            //    for (int Index = 0; Index < numbers.Length; Index++)
+            //    {
+            //        if (numbers[Index] <= MinValue)
+            //            MinValue = numbers[Index];
+            //    }
+            //    return MinValue;
+            //} 
+            #endregion
+        }
+    }
+}
